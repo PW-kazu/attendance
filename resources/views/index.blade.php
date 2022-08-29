f3baccbe48e39cad4297c5c9616a29820df1c087
PW kazu <komura_jutasrou@icloud.com>
2022-08-29 16:36:46 +0900

    修正

---
+++ resources/views/index.blade.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>attendance</title>
+  <style>
+    .atte{
+      color:black;
+      display: inline;
+      left: 100px;px
+    }
+    .home{
+      right: 600px;
+    }
+    .date{
+      right:700px
+    }
+    .logout{
+      right: 800px;
+    }
+
+  </style>
+</head>
+<body>
+  <div>
+    <p class="atte">Atte</p>
+    <a class="home" href="">ホーム</a>
+    <a class="date" href="">日付一覧</a>
+    <a class="logout" href="">ログアウト</a>
+  </div>
+</body>
+</html>
